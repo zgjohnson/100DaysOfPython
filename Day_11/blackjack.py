c58063fc7b4c71a060ece6aa5418a9cc60b17737
johnszg <zac.johnson@dillards.com>
2024-03-05 08:48:02 -0600

    My solution Day 11 without refactoring.

--- Day_11/blackjack.py
+++ Day_11/blackjack.py
@@ -1,60 +1,120 @@
-############### Blackjack Project #####################
+# ############## Blackjack Project #####################
+import random
 
 # Difficulty Normal 😎: Use all Hints below to complete the project.
 # Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
 # Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
 # Difficulty Expert 🤯: Only use Hint 1 to complete the project.
 
-############### Our Blackjack House Rules #####################
+# ############## Our Blackjack House Rules #####################
 
-## The deck is unlimited in size.
-## There are no jokers.
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
+# The deck is unlimited in size.
+# There are no jokers.
+# The Jack/Queen/King all count as 10.
+# The Ace can count as 11 or 1.
+# Use the following list as the deck of cards:
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# The cards in the list have equal probability of being drawn.
+# Cards are not removed from the deck as they are drawn.
+# The computer is the dealer.
 
-##################### Hints #####################
+# #################### Hints #####################
 
 # Hint 1: Go to this website and try out the Blackjack game:
 #   https://games.washingtonpost.com/games/blackjack/
 # Then try out the completed Blackjack project here:
 #   http://blackjack-final.appbrewery.repl.run
 
-# Hint 2: Read this breakdown of program requirements:
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-# Then try to create your own flowchart for the program.
+from blackjack_art import logo
 
-# Hint 3: Download and read this flow chart I've created:
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
 
-# Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-# 11 is the Ace.
-# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def get_random_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return cards[random.randint(0, len(cards) - 1)]
 
-# Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-# user_cards = []
-# computer_cards = []
 
-# Hint 6: Create a function called calculate_score() that takes a List of cards as input
-# and returns the score.
-# Look up the sum() function to help you do this.
+def draw_cards(number_of_cards, person):
+    for _ in range(number_of_cards):
+        card = get_random_card()
+        person['cards'].append(card)
+        person['total'] += card
 
-# Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
-# Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+def print_cards(player, house, final):
+    if final:
+        print(f"\tYour final hand: {player['cards']}, final score: {player['total']}")
+        print(f"\tComputer's final hand: {house['cards']}, final score: {house['total']}")
+    else:
+        print(f"\tYour Cards: {player['cards']}, current score: {player['total']}")
+        print(f"\tComputer's first card: {house['cards'][0]}")
 
-# Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
 
-# Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+def change_ace_value(person):
+    person['cards'].remove(11)
+    person['cards'].append(1)
+    person['total'] -= 10
 
-# Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
 
-# Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+keep_playing = True
 
-# Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+while keep_playing:
+    if input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") != 'y':
+        keep_playing = False
+        continue
 
-# Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+    print(logo)
+
+    player = {
+        'cards': [],
+        'total': 0
+    }
+
+    house = {
+        'cards': [],
+        'total': 0
+    }
+
+    draw_cards(2, player)
+    draw_cards(2, house)
+
+    print_cards(player, house, False)
+
+    if house['total'] == 21:
+        player_continue = False
+        house_auto_win = True
+    else:
+        player_continue = True
+        house_auto_win = False
+
+    while player_continue:
+        if input("Type 'y' to get another card, type 'n' to pass: ") == 'y':
+            draw_cards(1, player)
+            if player['total'] > 21:
+                if 11 in player['cards']:
+                    change_ace_value(player)
+                else:
+                    player_continue = False
+            print_cards(player, house, False)
+        else:
+            player_continue = False
+
+    if player['total'] <= 21:
+        while house['total'] < 17:
+            draw_cards(1, house)
+            if house['total'] > 21 and 11 in house['cards']:
+                change_ace_value(house)
+
+    print_cards(player, house, True)
+
+    if house_auto_win:
+        print("You lose.")
+    elif player['total'] > 21:
+        print("You busted. You lose.")
+    elif house['total'] > 21:
+        print("House busted. You win.")
+    elif player['total'] > house['total']:
+        print("You win.")
+    elif player['total'] == house['total']:
+        print("It's a draw.")
+    else:
+        print("You lose.")
