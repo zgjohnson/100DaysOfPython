92e5bf6b6fca26a66b4708e63d23516c0c597168
zgjohnson <zgjohnson@ualr.edu>
2024-07-25 17:23:12 -0500

    Day 34 complete

---
+++ Intermediate+/Day_34/quizzler-app/data.py
@@ -0,0 +1,6 @@
+import requests
+
+params = {'amount': 10, 'type': 'boolean', 'category': 18}
+response = requests.get(url='https://opentdb.com/api.php', params=params)
+response.raise_for_status()
+question_data = response.json()["results"]
